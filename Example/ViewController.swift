c09befa1b5053b91da683791369c0c2e1584f2dd
Xin Hong <xin@teambition.com>
2015-11-19 13:23:24 +0800

    Initial Commit

---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TBEmptyDataSet
+//
+//  Created by 洪鑫 on 15/11/19.
+//  Copyright © 2015年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TBEmptyDataSet/TBEmptyDataSet.swift
@@ -0,0 +1,9 @@
+//
+//  TBEmptyDataSet.swift
+//  TBEmptyDataSet
+//
+//  Created by Xin Hong on 15/11/19.
+//  Copyright © 2015年 Teambition. All rights reserved.
+//
+
+import UIKit
