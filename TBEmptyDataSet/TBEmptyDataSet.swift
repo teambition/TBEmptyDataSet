585384317db64175f72da0e0007f89a2d4a866a3
Xin Hong <xin@teambition.com>
2015-11-23 11:24:30 +0800

    empty data view

--- TBEmptyDataSet/TBEmptyDataSet.swift
+++ TBEmptyDataSet/TBEmptyDataSet.swift
@@ -10,14 +10,194 @@ import UIKit
 
 // MARK: - EmptyDataView
 private class EmptyDataView: UIView {
-    var view: UIView!
-    var imageView: UIImageView?
-    var titleLabel: UILabel?
-    var descriptionLabel: UILabel?
+    private struct Screen {
+        static let width = UIScreen.mainScreen().bounds.width
+        static let height = UIScreen.mainScreen().bounds.height
+    }
+    
+    private struct ViewStrings {
+        static let view = "view"
+        static let imageView = "imageView"
+        static let titleLabel = "titleLabel"
+        static let descriptionLabel = "descriptionLabel"
+    }
+    
+    private struct DefaultValues {
+        static let verticalSpace: CGFloat = 12
+    }
+    
+    // MARK: - Properties
+    lazy var view: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.clearColor()
+        view.userInteractionEnabled = true
+        view.alpha = 0
+        return view
+    }()
+    
+    lazy var imageView: UIImageView? = { [unowned self] in
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = UIColor.clearColor()
+        imageView.contentMode = .ScaleAspectFill
+        imageView.userInteractionEnabled = false
+        self.view.addSubview(imageView)
+        return imageView
+    }()
+    
+    lazy var titleLabel: UILabel? = { [unowned self] in
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.backgroundColor = UIColor.clearColor()
+        titleLabel.font = UIFont.systemFontOfSize(27)
+        titleLabel.textColor = UIColor(white: 0.6, alpha: 1)
+        titleLabel.textAlignment = .Center
+        titleLabel.lineBreakMode = .ByWordWrapping
+        titleLabel.numberOfLines = 0
+        self.view.addSubview(titleLabel)
+        return titleLabel
+    }()
+    
+    lazy var descriptionLabel: UILabel? = { [unowned self] in
+        let descriptionLabel = UILabel()
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.backgroundColor = UIColor.clearColor()
+        descriptionLabel.font = UIFont.systemFontOfSize(17)
+        descriptionLabel.textColor = UIColor(white: 0.6, alpha: 1)
+        descriptionLabel.textAlignment = .Center
+        descriptionLabel.lineBreakMode = .ByWordWrapping
+        descriptionLabel.numberOfLines = 0
+        self.view.addSubview(descriptionLabel)
+        return descriptionLabel
+    }()
     
     var tapGesture: UITapGestureRecognizer!
+    var customView: UIView? {
+        willSet {
+            if let _ = customView {
+                customView!.removeFromSuperview()
+            }
+        }
+        didSet {
+            customView!.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(customView!)
+        }
+    }
     
-    var customView: UIView?
+    var verticalSpace: CGFloat = DefaultValues.verticalSpace
+    
+    // MARK: - Helper
+    private func removeAllConstraints() {
+        view.removeConstraints(view.constraints)
+        removeConstraints(constraints)
+    }
+    
+    private func shouldShowImageView() -> Bool {
+        if let _ = imageView, _ = imageView?.superview {
+            return true
+        }
+        return false
+    }
+    
+    private func shouldShowTitleLabel() -> Bool {
+        if let _ = titleLabel, _ = titleLabel?.superview {
+            return titleLabel!.attributedText!.length > 0
+        }
+        return false
+    }
+    
+    private func shouldShowDescriptionLabel() -> Bool {
+        if let _ = descriptionLabel, _ = descriptionLabel?.superview {
+            return descriptionLabel!.attributedText!.length > 0
+        }
+        return false
+    }
+    
+    // MARK: - View life cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(self.view)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didMoveToSuperview() {
+        frame = super.bounds
+        UIView.animateWithDuration(0.25) { () -> Void in
+            self.view.alpha = 1
+        }
+    }
+    
+    func prepareForReuse() {
+        view.subviews.forEach { subview in
+            subview.removeFromSuperview()
+        }
+        imageView = nil
+        titleLabel = nil
+        descriptionLabel = nil
+        customView = nil
+        removeAllConstraints()
+    }
+    
+    // MARK: - Configuration
+    func setConstraints() {
+        let centerX = NSLayoutConstraint(item: view, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0)
+        let centerY = NSLayoutConstraint(item: view, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0)
+        addConstraint(centerX)
+        addConstraint(centerY)
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[\(ViewStrings.view)]|", options: [], metrics: nil, views: [ViewStrings.view : view]))
+        
+        if let _ = customView {
+            view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[\(ViewStrings.view)]|", options: [], metrics: nil, views: [ViewStrings.view : view]))
+            view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[\(ViewStrings.view)]|", options: [], metrics: nil, views: [ViewStrings.view : view]))
+        } else {
+            var viewStrings = [String]()
+            var views = [String: UIView]()
+            
+            if shouldShowImageView() {
+                let viewString = ViewStrings.imageView
+                viewStrings.append(viewString)
+                views[viewString] = imageView!
+                
+                view.addConstraint(NSLayoutConstraint(item: imageView!, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1, constant: 0))
+            }
+            
+            if shouldShowTitleLabel() {
+                let viewString = ViewStrings.titleLabel
+                viewStrings.append(viewString)
+                views[viewString] = titleLabel!
+                view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[\(ViewStrings.titleLabel)(>=0)]-|", options: [], metrics: nil, views: [ViewStrings.titleLabel : titleLabel!]))
+            } else {
+                titleLabel?.removeFromSuperview()
+                titleLabel = nil
+            }
+            
+            if shouldShowDescriptionLabel() {
+                let viewString = ViewStrings.descriptionLabel
+                viewStrings.append(viewString)
+                views[viewString] = descriptionLabel!
+                view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[\(ViewStrings.descriptionLabel)(>=0)]-|", options: [], metrics: nil, views: [ViewStrings.descriptionLabel : descriptionLabel!]))
+            } else {
+                descriptionLabel?.removeFromSuperview()
+                descriptionLabel = nil
+            }
+            
+            var verticalFormat = String()
+            for (index, viewString) in viewStrings.enumerate() {
+                verticalFormat += "[\(viewString)]"
+                if index != viewStrings.count - 1 {
+                    verticalFormat += "-\(verticalSpace)-"
+                }
+            }
+            
+            if !verticalFormat.isEmpty {
+                view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|\(verticalFormat)|", options: [], metrics: nil, views: views))
+            }
+        }
+    }
 }
 
 // MARK: - TBEmptyDataSetDataSource
@@ -46,13 +226,9 @@ protocol TBEmptyDataSetDelegate: NSObjectProtocol {
     func emptyDataSetDidDisappear(scrollView: UIScrollView!)
 }
 
+// MARK: - Extension
 extension UIScrollView: UIGestureRecognizerDelegate {
     // MARK: - Structs
-    private struct Screen {
-        static let width = UIScreen.mainScreen().bounds.width
-        static let height = UIScreen.mainScreen().bounds.height
-    }
-    
     private struct AssociatedKeys {
         static let emptyDataSetDataSource = "emptyDataSetDataSource"
         static let emptyDataSetDelegate = "emptyDataSetDelegate"
@@ -96,11 +272,25 @@ extension UIScrollView: UIGestureRecognizerDelegate {
         get {
             return objc_getAssociatedObject(self, AssociatedKeys.emptyDataSetDataSource) as? TBEmptyDataSetDataSource
         }
+        set {
+            if newValue == nil || !emptyDataSetAvailable() {
+                handlingInvalidEmptyDataSet()
+            }
+            
+            objc_setAssociatedObject(self, AssociatedKeys.emptyDataSetDataSource, self.emptyDataSetDataSource, .OBJC_ASSOCIATION_ASSIGN)
+        }
     }
     
     var emptyDataSetDelegate: TBEmptyDataSetDelegate? {
         get {
             return objc_getAssociatedObject(self, AssociatedKeys.emptyDataSetDelegate) as? TBEmptyDataSetDelegate
+        }
+        set {
+            if newValue == nil {
+                handlingInvalidEmptyDataSet()
+            }
+            
+            objc_setAssociatedObject(self, AssociatedKeys.emptyDataSetDelegate, self.emptyDataSetDelegate, .OBJC_ASSOCIATION_ASSIGN)
         }
     }
     
@@ -131,7 +321,7 @@ extension UIScrollView: UIGestureRecognizerDelegate {
     }
     
     // MARK: - Setters
-    private func setEmptyDataView(emptyDataView: EmptyDataView) {
+    private func setEmptyDataView(emptyDataView: EmptyDataView?) {
         objc_setAssociatedObject(self, AssociatedKeys.emptyDataView, emptyDataView, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
     
@@ -317,6 +507,17 @@ extension UIScrollView: UIGestureRecognizerDelegate {
         }
         
         return count
+    }
+    
+    private func handlingInvalidEmptyDataSet() {
+        emptyDataSetWillDisappear()
+        
+        emptyDataView.prepareForReuse()
+        emptyDataView.removeFromSuperview()
+        setEmptyDataView(nil)
+        scrollEnabled = true
+        
+        emptyDataSetDidDisappear()
     }
 }
 
