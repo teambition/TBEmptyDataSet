7e235e507423eaa37fd0a5e02b362c6f93577a89
thejeff77 <unspecified@unspecified.com>
2021-01-02 19:04:11 -0700

    Adding folder path to needed files

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TBEmptyDataSet",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "TBEmptyDataSetTests",
-            dependencies: ["TBEmptyDataSet"]),
+            dependencies: ["TBEmptyDataSet"],
+            path: "Tests"),
     ]
 )
